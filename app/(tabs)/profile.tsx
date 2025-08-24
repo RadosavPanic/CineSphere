45975cd8599311e139e76b6fd8dd10cbfd48c733
RadosavPanic <radosav.panic@gmail.com>
2025-08-24 16:34:14 +0200

    customized homepage and integrated TMDB api to display movies, created custom useFetch hook

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-light-100">Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
 
-const App = () => {
+const Saved = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Welcome</Text>
+      <Text className="text-5xl text-light-200">Saved</Text>
     </View>
   );
 };
 
-export default App;
+export default Saved;
